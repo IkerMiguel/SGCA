57186bc3b841dd30708d4e69942fb194762a1c37
IkerMiguel <ikmiguel03@gmail.com>
2025-04-09 22:09:08 -0500

    Migrations

---
+++ database/migrations/2025_04_10_024028_create_vehiculos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use SebastianBergmann\CodeCoverage\Report\Xml\Totals;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->id();
+            $table->string('marca', 255);
+            $table->string('modelo', 255);
+            $table->year('año');
+            $table->decimal('precio', total:8, places:2);
+            $table->integer('kilometraje');
+            $table->enum('tipo', ['sedan', 'suv', 'coupe']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+};
---
+++ database/migrations/2025_04_10_024102_create_ventas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vehiculo_id')->constrained('vehiculos')->onDelete('cascade');
+            $table->foreignId('cliente_id')->constrained('clientes')->onDelete('cascade');
+            $table->date('fecha_venta');
+            $table->decimal('precio_final',total:8,places:2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
